48fb6d7712d270cd87b7dbdd7a416eb03a4a679b
Magnus Mueller <mamuelle@informatik.hu-berlin.de>
2011-05-16 08:27:08 +0200

    added orm mongoid

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,9 @@ module GridFS
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    config.generators do |g|
+      g.orm :mongoid
+    end
   end
 end
