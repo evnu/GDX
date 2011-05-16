d9c531b6fafa50989768e6c0d09b184335ec2bef
Magnus Mueller <mamuelle@informatik.hu-berlin.de>
2011-05-16 08:15:29 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GridFS::Application.config.secret_token = 'fafa81c16054c925af2aff88d216792b437bf4bc246f7d5783e8d13efd4009118ce3546764c181902f6ea3e0842552a967664f8edcd13766808240a80acc2e57'
