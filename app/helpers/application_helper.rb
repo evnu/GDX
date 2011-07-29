dcf3ed6cc6f02de93d31b25e386623096a3c699b
Magnus Mueller <mamuelle@informatik.hu-berlin.de>
2011-07-29 09:39:33 +0200

    highlight links in menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def highlight_link(page_name)
+    if params[:controller] == page_name
+      "current_page_item"
+    else
+      "page_item page-item-2"
+    end
+  end
 end
