ba8c69aa37bd6f2fb27018dd461c4c6bdcbf6ec3
Magnus Mueller <mamuelle@informatik.hu-berlin.de>
2011-09-08 21:01:22 +0200

    simple paging.

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -28,28 +28,32 @@ class Upload
 
   def self.paginate_array(array, params)
     per_page = params[:per_page]
-    per_page = 20 unless per_page
+    per_page = 10 unless per_page
 
     # use only the results in the current page
     # NOTE: this might be a huge performance hit. Why should we first 
     # get all values and then throw most of them away?
     # NOTE: we have to determine the number of pages before doing this, because
     # we have to use the total number of results
-    num_pages = array.count / per_page 
+    num_pages = (array.count.to_f / per_page).ceil
     @paginated_array = array[per_page*params[:page].to_i..per_page*(params[:page].to_i+1)] if params[:page]
     @paginated_array = array[0..per_page] unless params[:page]
+
+    if not @paginated_array
+      @paginated_array = []
+    end
 
     # make kaminari work with an array
     # see http://code.dblock.org/mongoid-202-dropped-pagination-kaminari
     @paginated_array.instance_eval <<-EVAL
         def current_page
-    #{params[:page] || 1}
+          #{params[:page] || 1}
         end
         def num_pages
-    #{num_pages}
+          #{num_pages}
         end
         def limit_value                                                                               
-          20
+          #{per_page}
         end
     EVAL
 
