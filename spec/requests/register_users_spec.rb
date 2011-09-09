dda8498b73c35ff52f2df590a871e67eb91bdaa9
Magnus Mueller <mamuelle@informatik.hu-berlin.de>
2011-09-09 14:16:02 +0200

    test for register request

--- spec/requests/register_users_spec.rb
+++ spec/requests/register_users_spec.rb
@@ -2,10 +2,11 @@ require 'spec_helper'
 
 describe "RegisterUsers" do
   describe "GET /register_users" do
-    it "works! (now write some real specs)" do
+    it "should return unauthorized(302) when not logged in as admin" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       get register_users_path
-      response.status.should be(200)
+      response.status.should be(302)
     end
+    pending "should return success(200) when logged in as admin"
   end
 end
