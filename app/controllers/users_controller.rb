bc1b5860fd85737bef6c4b3dfb47efa78016a15a
Magnus Mueller <mamuelle@informatik.hu-berlin.de>
2011-09-20 23:11:51 +0200

    use the same style to show the list of uploads

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
     if params[:id]
       @user = User.find(params[:id])
       @paginated_uploads = Upload.paginate_array @user.upload, params
+      @uploads = @user.upload
     else
       flash[:error]  = "No user given"
     end
